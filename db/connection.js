b16f714fb64921571f55d56aeac8a6f0f2fb0c29
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2020-05-14 16:22:03 +1000

    separate connection into its own file

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+import Nano from 'nano';
+
+const connection = (function connect() {
+  const host = process.env['COUCHDB_HOST'] || 'localhost:5984';
+  const username = process.env['COUCHDB_USERNAME'] || 'admin';
+  const password = process.env['COUCHDB_PASSWORD'] || 'admin';
+  return Nano(`http://${username}:${password}@${host}`);
+})()
+
+export default connection;
