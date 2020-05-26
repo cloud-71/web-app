ed48f0026a38dd5111e534e59052d731dc9d6b03
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2020-05-27 03:36:00 +1000

    Debug couchDB connection

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,7 @@ const connection = (function connect() {
   const host = process.env['COUCHDB_HOST'] || 'localhost:5984';
   const username = process.env['COUCHDB_USERNAME'] || 'admin';
   const password = process.env['COUCHDB_PASSWORD'] || 'admin';
+  console.log(`Connecting to CouchDB ${host}, username: ${username}, password ${password}`);
   return Nano(`http://${username}:${password}@${host}`);
 })();
 
