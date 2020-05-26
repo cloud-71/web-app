748b7f53207d3aed1090821ee8caaf394c6fe2f6
Brody Taylor <taylbr97@gmail.com>
2020-05-27 03:45:36 +1000

    im tired

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,12 @@ import Nano from 'nano';
 const connection = (function connect() {
   const host = process.env['COUCHDB_HOST'] || 'localhost:5984';
   const username = process.env['COUCHDB_USERNAME'] || 'admin';
-  const password = process.env['COUCHDB_PASSWORD'] || 'admin';
-  console.log(`Connecting to CouchDB ${host}, username: ${username}, password ${password}`);
+  //const password = process.env['COUCHDB_PASSWORD'] || 'admin';
+
+  const password = 'GnBLYZgVcfHyEYtVfP8y';
+  console.log(
+    `Connecting to CouchDB ${host}, username: ${username}, password ${password}`,
+  );
   return Nano(`http://${username}:${password}@${host}`);
 })();
 
